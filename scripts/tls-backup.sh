43a74da4ad9461b84d836fde9a889edf4cc25e9f
Ilya Sotkov <ilya@sotkov.com>
2018-03-30 22:01:16 +0300

    Fix tls-backup script

--- scripts/tls-backup.sh
+++ scripts/tls-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-domain_zone="flexeption.pw"
-source=("ci" "registry" "charts" "staging" "production")
+domain_zone="swarm.pw"
+source=("status")
 green="\e[1;32m"
 no_color="\e[0m"
 secret_file_name="certs-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
@@ -9,6 +9,6 @@ secret_file_name="certs-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
 for i in ${source[@]}
 do
     echo "---" >> ${TF_VAR_secrets_dir}/kube-system/kube-lego/${secret_file_name} \
-    && kubectl get secret $i.$domain_zone-tls -o yaml >> ${TF_VAR_secrets_dir}/kube-system/kube-lego/${secret_file_name} \
+    && kubectl -n team1 get secret $i.$domain_zone-tls -o yaml >> ${TF_VAR_secrets_dir}/kube-system/kube-lego/${secret_file_name} \
     && printf "${green}Secret $i.$domain_zone-tls has been saved to ${TF_VAR_secrets_dir}/kube-system/${secret_file_name} successfully!\n${no_color}"
 done
