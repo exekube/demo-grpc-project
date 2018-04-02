330ea3f39de5b833ad81abbd9d2fb5858376c050
Ilya Sotkov <ilya@sotkov.com>
2018-04-02 11:17:19 +0300

    Fix bug with "_helm" path in alias-helm script

--- scripts/alias-helm.sh
+++ scripts/alias-helm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ns=team1
+ns=kube-system
 
 alias helm="helm --tiller-namespace $ns \
 --tls --tls-verify \
---tls-ca-cert $TF_VAR_secrets_dir/$ns/helm-tiller/ca.cert.pem \
---tls-cert $TF_VAR_secrets_dir/$ns/helm-tiller/helm.cert.pem \
---tls-key $TF_VAR_secrets_dir/$ns/helm-tiller/helm.key.pem"
+--tls-ca-cert $TF_VAR_secrets_dir/$ns/_helm/ca.cert.pem \
+--tls-cert $TF_VAR_secrets_dir/$ns/_helm/helm.cert.pem \
+--tls-key $TF_VAR_secrets_dir/$ns/_helm/helm.key.pem"
